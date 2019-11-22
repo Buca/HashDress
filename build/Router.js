6a060d7fd5b5a8d4496ca3d983c2b5b6447da204
Buca Hajdini <bucahajdini@gmail.com>
2019-11-22 12:19:25 +0200

    Updated to v0.1.2-alpha

--- build/Router.js
+++ build/Router.js
@@ -1,385 +1,496 @@
-const Path = require( './Path.js' ),
-	  Query = require( './Query.js' );
+function Router ( ) {
 
-function Router ( options = {} ) {
+	EventMock.call( this );
 
-	// Options
-	this.autoInit = options.autoInit !== undefined ? options.autoInit : false;
+	this.hash = {
 
-	// Hash/Router
-	this.routerEnabled = options.routerEnabled || true;
-	//this.listenToHashChanges = true;
-	this.currentHash = undefined;
+		current: {
+			string: null
+		},
+		enabled: true,
+		deltaTrigger: false
 
-	// Paths
-	this.pathsEnabled = true;
-	this.paths = new Set();
-	this.deltaTriggerPath = true;
-	this.currentPath = {
+	};
 
-		path: undefined,
+	this.paths = {
+
+		current: {
+			string: '',
+			list: [],
+			parameters: {}
+		},
 		list: [],
-		parameters: {}
+		enabled: true,
+		deltaTrigger: false
 
 	};
 
-	// Query
-	this.queriesEnabled = true;
-	this.queries = new Map();
-	this.deltaTriggerQueries = true;
-	this.currentQueries = {
+	this.queries = {
 
-		queries: undefined,
-		list: new Map()
+		current: {
+			string: '',
+			list: []
+		},
+		enabled: true,
+		deltaTrigger: false,
+		list: []
 
 	};
 
-	// Fragment
-	this.fragmentEnabled = true;
-	this.currentFragment = undefined;
+	this.fragment = {
+
+		current: {
+			string: '',
+			value: ''
+		},
+		deltaTrigger: false
+
+	};
 
 	// Constructors
-	this.Path = Path;
-	this.Query = Query;
+	this.Path = Path.bind( null, this );
+	this.Query = Query.bind( null, this );
 	this.Router = Router;
 
-	if( this.autoInit ) {
+	// Hash Change
+	this.on( 'hash-change', function( hash ) {
 
-		this.init();
+		this.hash.current.string = hash.string;
 
-	}
+		// If: deltaTriggering is false or the paths are not equal.
+		if( this.paths.enabled && ( !this.paths.deltaTrigger || 
+			hash.paths.string !== this.paths.current.string ) ) {
+
+			this.dispatch( 'path-change', hash.path );
+
+		}
+
+		// If: deltaTriggering is false or the paths are not equal.
+		if( this.queries.enabled && ( !this.queries.deltaTrigger || 
+			hash.queries.string !== this.queries.current.string ) ) {
+
+			this.dispatch( 'queries-change', hash.queries );
+
+		}
+
+		// If: deltaTriggering is false or the paths are not equal.
+		if( this.fragment.enabled && ( !this.fragment.deltaTrigger || 
+			hash.fragment.string !== this.fragment.current.string ) ) {
+
+			this.dispatch( 'fragment-change', hash.fragment );
+
+		}
+
+	} );
+
+	this.on( 'path-change', function( path ) {
+
+		this.paths.current = path;
+
+		this.triggerPaths( path );
+
+	} );
+
+	this.on( 'queries-change', function( queries ) {
+
+		this.queries.current = queries;
+
+		this.triggerQueries( queries );
+
+	} );
+
+	this.on( 'fragment-change', function( fragment ) {
+
+		this.fragment.current = fragment;
+
+		this.triggerFragment( fragment );
+
+	} );
+
+	var hashChange = function() {
+
+		if( this.hash.enabled ) {
+
+			var hash = Utils.parseHash( window.location.hash );
+
+			if( this.hash.enabled && ( !this.hash.deltaTrigger || 
+				hash.string !== this.hash.current.string ) ) {
+
+				this.dispatch( 'hash-change', hash );
+
+			}
+
+		}
+
+	}.bind( this );
+
+	window.addEventListener( 'hashchange', hashChange );
+
+	hashChange();
 
 };
 
-Router.prototype = {
+Router.prototype = Object.assign( Object.create( EventMock.prototype ), {
 
-	_hashChange: function( hash, _historyMethod ) {
+	constructor: Router,
 
-		if( this.currentHash !== hash && this.routerEnabled ) {
+	// Hash
+	setHash: function( hash, historyMethod = 'replace', trigger = true ) {
 
-			var list,
-				path, query, fragment;
+		hash = Utils.parseHash( hash );
 
-			list = hash.split( '?' );
+		Utils.history( 
+			historyMethod, 
+			hash.string
+		);
 
-			if( list.length > 1 ) {
+		if( trigger ) {
 
-				list.push( ...list.pop().split( '#' ) );
-				path = list[ 0 ];
-				query = list[ 1 ];
-				fragment = list.length > 2 ? list[ 2 ] : undefined;
+			if( this.hash.enabled ) {
 
-			}
+				if( this.hash.enabled && ( !this.hash.deltaTrigger || 
+					hash.string !== this.hash.current.string ) ) {
 
-			else {
+					this.dispatch( 'hash-change', hash );
 
-				list = list[ 0 ].substring( 1 ).split( '#' );
-				path = '#' + list[ 0 ];
-				query = undefined;
-				fragment = list.length > 1 ? list[ 1 ] : undefined;
-
-			}
-
-			path = this._pathChange( path );
-			query = this._queryChange( query );
-			fragment = this._fragmentChange( fragment  ); 
-
-			this.currentHash = path + query + fragment;
-
-			if( _historyMethod === 'replace' ) {
-
-				history.replaceState( null, null, this.currentHash );
-
-			}
-
-			else if( _historyMethod === 'push' ) {
-
-				history.pushState( null, null, this.currentHash )
+				}
 
 			}
 
 		}
 
+		return this;
+
 	},
 
-	_pathChange: function( path ) {
+	getHash: function() {
 
-		path = this.fixPath( path );
+		return this.hash.current.string;
 
-		if( ( this.currentPath.path !== path ||
-			!this.deltaTriggerPath ) && this.pathsEnabled ) {
+	},
 
-			this.triggerPath( path );
+	enable: function() {
 
-			this.currentPath.path = path;
+		this.hash.enabled = true;
+
+		return this;
+
+	},
+
+	disable: function() {
+
+		this.hash.enabled = false;
+
+		return this;
+
+	},
+
+
+	// Path
+	triggerPaths: function( path ) {
+
+		path = Utils.parsePath( path );
+
+		if( !this.paths.enabled ) return this;
+
+		for( var i = this.paths.list.length - 1; 0 <= i; i -- ) {
+
+			var regPath = this.paths.list[ i ];
+
+			if( regPath.disposed ) this.paths.list.splice( i, 1 );
+
+			var valid = regPath.validate( path );
+
+			if( valid !== false ) {
+
+				valid = Object.assign( valid, path );
+
+				regPath.trigger( valid );
+
+				if( !regPath.entered ) {
+
+					regPath.dispatch( 'enter', valid );
+
+				}
+
+			}
+
+			else if( regPath.entered ) {
+
+				regPath.dispatch( 'leave', path );
+
+			}
 
 		}
 
-		return path;
+		return this;
 
 	},
 
-	_queryChange: function( queries ) {
+	setPath: function( path, historyMethod = 'replace', trigger = true ) {
 
-		queries = queries !== undefined ? this.fixQueries( queries ) : '';
+		path = Utils.parsePath( path );
 
-		if( ( !this.deltaTriggerQueries ||
-			this.currentQueries !== queries ) &&
-			this.queriesEnabled ) {
+		Utils.history( 
+			historyMethod, 
+			path.string + this.queries.current.string + this.fragment.current.string
+		);
 
+		if( trigger ) {
+
+			this.triggerPaths( path );
+
+		}
+
+		return this;
+
+	},
+
+	getPath: function() {
+
+		return this.paths.current.string;
+
+	},
+
+	enablePaths: function() {
+
+		this.paths.enabled = true;
+
+		return this;
+
+	},
+
+	disablePaths: function() {
+
+		this.paths.enabled = false;
+
+		return this;
+
+	}, 
+
+
+	// Queries
+	triggerQueries: function( queries ) {
+
+		if( !this.queries.enabled ) return this;
+
+		queries = Utils.parseQueries( queries );
+
+		for( var i = 0; i < queries.list.length; i ++ ) {
+
+			for( var j = this.queries.list.length - 1; 0 <= j; j -- ) {
+
+				var regQuery = this.queries.list[ j ];
+
+				if( regQuery.disposed ) this.paths.list.splice( j, 1 );
+
+				var valid = regQuery.validate( queries.list[ i ] );
+
+				if( valid !== false ) {
+
+					valid = Object.assign( valid, queries.list[ i ] );
+
+					regQuery.trigger( valid );
+
+				}
+
+			}
+
+		}
+
+		return this;
+
+	},
+
+	setQueries: function( queries, historyMethod = 'replace', trigger = true ) {
+
+		queries = Utils.parseQueries( path );
+
+		Utils.history( 
+			historyMethod, 
+			this.paths.current.string + queries.string + this.fragment.current.string
+		);
+
+		if( trigger ) {
 
 			this.triggerQueries( queries );
 
-			this.currentQueries.queries = queries;
-
-		}
-
-		return queries;
-
-	},
-
-	_fragmentChange: function( fragment ) {
-
-		if( this.fragmentEnabled && fragment !== undefined ) {
-
-			this.triggerFragment( fragment );
-			this.currentFragment = fragment;
-
-		}
-
-		return fragment !== undefined ? '#' + fragment : '';
-
-	},
-
-	setHash: function() {},
-
-	setPath: function() {},
-
-	setQuery: function() {},	
-
-	fixPath: function( path ) {
-
-		if( path.length > 1 ) {
-
-			if( path[ 1 ] !== '/' ) {
-
-				path = '#/' + path.substring( 1 );
-
-			}
-
-			else {
-
-				path = '#/' + path.substring( 2 );
-
-			}
-
-			if( path.length > 2 && path[ path.length - 1 ] === '/' ) {
-
-				path = path.substring( 0, path.length - 1 );
-
-			}
-
-		}
-
-		else {
-
-			path = '#/';
-
-		}
-
-		return path;
-
-	},
-
-	parsePath: function( path ) {
-
-		// Path is assumed to be fixed.
-
-		var list = path.split( '/' ),
-			parameters = {};
-
-		for( var i = 1; i < list.length; i ++ ) {
-
-			if( list[ i ].indexOf( ':' ) !== -1 ) {
-
-				parameters[ list[ i ].substring( 1 ) ] = {
-
-					index: i
-
-				};
-
-			}
-
-			else if( list[ i ] === '' ) {
-
-				list.splice( i, 1 );
-
-			}
-
-		}
-
-		return {
-
-			path: path,
-			list: list,
-			parameters: parameters
-			
-		};
-
-	},
-
-	triggerPath: function( path ) {
-
-		// Path is assumed to be fixed.
-
-		// Parametric paths.
-		var list = path.split( '/' );
-
-		this.paths.forEach( function( entry ) {
-
-			if( list.length === entry.list.length ||
-				( entry.wild && entry.list.length <= list.length ) ) {
-
-				for( var j = 0; j < entry.list.length; j ++ ) {
-
-					if( entry.list[ j ][ 0 ] !== ':' &&
-						list[ j ] !== entry.list[ j ] &&
-						!( j === entry.list.length - 1 && entry.wild && entry.list[ j ] === list[ j ].substring( 0, entry.list ) ) ) {
-
-						break;
-
-					}
-
-				}
-
-				if( j === entry.list.length ) {
-
-					var parameters = {};
-
-					for( var param in entry.parameters ) {
-
-						parameters[ param ] = 
-							list[ entry.parameters[ param ].index ];
-
-					}
-
-					entry.callback( path, parameters );
-
-				}
-
-			}
-
-		} );
-
-	},
-
-	fixQueries: function( query ) {
-
-		if( query[ 0 ] !== '?' ) {
-
-			query = '?' + query;
-
-		}
-
-		return query;
-
-	},
-
-	parseQueries: function( query ) {
-
-		var list = query.split( '&' );
-
-		list[ 0 ] = list[ 0 ].substring( 1 );
-
-		for( var i = 0; i < list.length; i ++ ) {
-
-			list[ i ] = list[ i ].split( '=' );
-
-		}
-
-		return list;
-
-	},
-
-	triggerQueries: function( queries ) {
-
-		// remove queries
-
-		queries = this.parseQueries( queries );
-
-		var query;
-
-		for( var i = 0; i < queries.length; i ++ ) {
-
-			query = this.queries.get( queries[ i ][ 0 ] );
-
-			if( query !== undefined && query.enabled ) {
-
-				if( !this.deltaTriggerQueries ||
-					query.value !== queries[ i ][ 1 ] ||
-					!this.currentQueries.list.has( query.name ) ) {
-
-					this.currentQueries.list.set( query.name, query );
-
-					query.value = queries[ i ][ 1 ];
-					query.method( query.value );
-
-				}
-
-			}
-
 		}
 
 	},
 
+	getQueries: function() {
+
+		return this.queries.current.string;
+
+	},
+
+
+	// Fragment
 	triggerFragment: function( fragment ) {
 
-		if( document.readyState === 'complete' ||
-			document.readyState === 'interactive' ) {
+		if( this.fragment.enabled ) {
 
-		    var element = document.getElementById( fragment );
+			fragment = Utils.parseFragment( fragment );
 
-			if( element !== null ) {
+			this.fragment.current = fragment;
 
-				element.scrollIntoView();
+			if( document.readyState === 'complete' ) {
 
-			}
-
-		}
-
-		else {
-
-		    window.addEventListener( 'DOMContentLoaded', function() {
-
-		        var element = document.getElementById( fragment );
+			    var element = document.getElementById( fragment.value );
 
 				if( element !== null ) {
 
 					element.scrollIntoView();
+
 				}
 
-		    } );
+			}
+
+			else {
+
+			    window.addEventListener( 'DOMContentLoaded', function() {
+
+			        var element = document.getElementById( fragment );
+
+					if( element !== null ) {
+
+						element.scrollIntoView();
+					}
+
+			    } );
+
+			}
 
 		}
 
 	},
 
-	init: function() {
+	setFragment: function( fragment, historyMethod = 'replace', trigger = true ) {
 
-		this._hashChange( window.location.hash, 'replace' );
+		fragment = Utils.parseFragment( fragment );
 
-		var hashChange = function() {
+		Utils.history( 
+			historyMethod, 
+			this.paths.current.string + this.queries.current.string + fragment.string
+		);
 
-			this._hashChange( window.location.hash, 'replace' );
+		if( trigger ) {
 
-		};
-		
-		hashChange = hashChange.bind( this );
+			this.triggerFragment( path );
 
-		window.addEventListener( 'hashchange', hashChange );
+		}
+
+	},
+
+	getFragment: function() {
+
+		return this.fragment.current.string;
+
+	},
+
+
+	// Detach & Attach
+	detach: function( instance ) {
+
+		if( instance === undefined ) {
+
+			this.detach( this.paths.list );
+			this.detach( this.queries.list );
+
+		}
+
+		else if( !Utils.isArray( instance ) ) instance = [ instance ];
+
+		if ( Utils.isArray( instance ) ) {
+
+			var index;
+
+			for( var i = 0; i < instance.length; i ++ ) {
+
+				if( instance[ i ] instanceof Path ) {
+
+					index = this.paths.list.indexOf( instance[ i ] );
+
+					if( index !== -1 )  { 
+
+						this.paths.splice( index, 1 );
+						instance.detach( this, false );
+
+					}
+
+				}
+
+				else if( instance instanceof Query ) {
+
+					index = this.queries.list.indexOf( instance[ i ] );
+
+					if( index !== -1 ) this.queries.splice( index, 1 );
+
+				}
+
+				else throw TypeError( 'The argument needs to be an instance of Path or Query or array of Paths and Queries.' );
+
+			}
+
+		}
+
+	},
+
+	attach: function( instance ) {
+
+		if( !Utils.isArray( instance ) ) instance = [ instance ];
+
+		if( Utils.isArray( instance ) ) {
+
+			var index;
+
+			for( var i = 0; i < instance.length; i ++ ) {
+
+				if( instance[ i ] instanceof Path ) {
+
+					index = this.paths.list.indexOf( instance[ i ] );
+
+					if( index === -1 ) this.paths.list.push( instance[ i ] );
+
+				}
+
+				else if( instance[ i ] instanceof Query ) {
+
+					index = this.queries.list.indexOf( instance[ i ] );
+
+					if( index === -1 ) this.queries.list.push( instance[ i ] );
+
+				}
+
+				else throw TypeError( 'The argument needs to be an instance of Path or Query or array of Paths and Queries.' );
+
+			}
+
+		}
+
+	},
+
+
+	// Dispose
+	dispose: function( detach = false ) {
+
+		this.clearEventListeners();
+
+		if( detach ) {
+
+			this.detach();
+
+		}
+
+		for( var props in this ) {
+
+			this[ props ] = undefined;
+
+		}
+
+		return undefined;
 
 	}
 
-};
-
-module.exports = Router;
+} );
